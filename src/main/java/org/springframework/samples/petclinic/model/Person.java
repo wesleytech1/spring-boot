e9cdb0739dfe3f4de33b441215346df451989b05
wesleytech1 <164554932+wesleytech1@users.noreply.github.com>
2024-05-11 02:37:46 -0300

    Update Person.java

--- src/main/java/org/springframework/samples/petclinic/model/Person.java
+++ src/main/java/org/springframework/samples/petclinic/model/Person.java
@@ -25,7 +25,7 @@ import javax.validation.constraints.NotEmpty;
  * @author Ken Krebs
  */
 @MappedSuperclass
-public class Person extends BaseEntity1 {
+public class Person extends BaseEntity {
 
 	@Column(name = "first_name")
 	@NotEmpty
